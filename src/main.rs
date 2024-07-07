4c677230f1f38d4b6cc1b45292c5e8a7efe63f6c
zhangfengcdt <feng@wherobots.com>
2024-07-07 11:37:06 -0700

    add prollytree 0.1.0 dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 use clap::Parser;
 use prollytree::config::TreeConfig;
 use prollytree::storage::InMemoryNodeStorage;
@@ -70,5 +84,63 @@ fn main() {
             }
         }
         Cli::Proof { .. } | Cli::Diff { .. } => todo!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn demo() {
+        // 1. Create a custom tree config
+        let config = TreeConfig {
+            base: 131,
+            modulus: 1_000_000_009,
+            min_chunk_size: 4,
+            max_chunk_size: 8 * 1024,
+            pattern: 0b101,
+            root_hash: None,
+        };
+
+        // 2. Create and Wrap the Storage Backend
+        let storage = InMemoryNodeStorage::<32>::new();
+
+        // 3. Create the Prolly Tree
+        let mut tree = ProllyTree::new(storage, config);
+
+        // 4. Insert New Key-Value Pairs
+        tree.insert(b"key1".to_vec(), b"value1".to_vec());
+        tree.insert(b"key2".to_vec(), b"value2".to_vec());
+
+        // 5. Traverse the Tree with a Custom Formatter
+        let traversal = tree.formatted_traverse(|node| {
+            let keys_as_strings: Vec<String> =
+                node.keys.iter().map(|k| format!("{:?}", k)).collect();
+            format!("[L{}: {}]", node.level, keys_as_strings.join(", "))
+        });
+        println!("Traversal: {}", traversal);
+
+        // 6. Update the Value for an Existing Key
+        tree.update(b"key1".to_vec(), b"new_value1".to_vec());
+
+        // 7. Find or Search for a Key
+        if let Some(node) = tree.find(b"key1") {
+            println!("Found key1 with value: {:?}", node);
+        } else {
+            println!("key1 not found");
+        }
+
+        // 8. Delete a Key-Value Pair
+        if tree.delete(b"key2") {
+            println!("key2 deleted");
+        } else {
+            println!("key2 not found");
+        }
+
+        // 9. Print tree stats
+        println!("Size: {}", tree.size());
+        println!("Depth: {}", tree.depth());
+        println!("Summary: {}", tree.summary());
     }
 }
